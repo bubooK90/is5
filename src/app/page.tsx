779dd2ce2511c5a60c747aa684ac20d1d281ab29
Beijing Uncle <kevin.p.gao@gmail.com>
2025-02-15 18:59:32 -0500

    20250215

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,105 @@
-import Image from "next/image";
+'use client';
+
+import { useEffect, useState, useRef, useCallback } from 'react';
+import Masonry from "react-masonry-css";
+
+const MAX_PAGES = 6; // 设置最大页面数常量
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [items, setItems] = useState([]);
+  const [page, setPage] = useState(MAX_PAGES);
+  const [hasMore, setHasMore] = useState(true);
+  const [selectedItem, setSelectedItem] = useState(null);
+  const observer = useRef<IntersectionObserver | null>(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    async function fetchData(page: number) {
+      const response = await fetch(`/data/page_${page}.json`); // 替换为你的 API 端点
+      const data = await response.json();
+      setItems(prevItems => [...prevItems, ...data]);
+      if (page < 2) {
+        setHasMore(false);
+      }
+    }
+
+    fetchData(page);
+  }, [page]);
+
+  const lastItemRef = useCallback(node => {
+    if (observer.current) observer.current.disconnect();
+    observer.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && hasMore) {
+        setPage(prevPage => prevPage - 1);
+      }
+    });
+    if (node) observer.current.observe(node);
+  }, [hasMore]);
+
+  const breakpointColumnsObj = {
+    default: 5,
+    1024: 4,
+    768: 3,
+    640: 2
+  };
+
+  const handleCardClick = (item) => {
+    setSelectedItem(item);
+  };
+
+  const handleClose = () => {
+    setSelectedItem(null);
+  };
+
+  return (
+    <main className="py-5 px-5 sm:px-10 md:px-15 lg:px-20">
+      <Masonry breakpointCols={breakpointColumnsObj} className="my-masonry-grid" columnClassName="my-masonry-grid_column">
+        <div key="intelli.show" className='p-5 bg-stone-900/80 text-stone-100/80 rounded-lg'>
+          <div className='mt-2 text-xl font-bold'>imagined: <span className='text-stone-100/50'>reality</span></div>
+          <div className='mb-20 text-xs text-stone-100/30'>niji・journey</div>
+          <div className='text-right text-xs text-stone-100/30'>kevin.p.gao@gmail.com</div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        {items.map((item, index) => {
+          if (index === items.length - 1) {
+            return (
+              <div ref={lastItemRef} key={index} onClick={() => handleCardClick(item)}>
+                <img
+                  className="lazy rounded-lg cursor-pointer"
+                  src={`https://cdn.midjourney.com/${item.jobid}/${item.index}.png`}
+                  alt={`${item.prompt}`}
+                  loading="lazy"
+                />
+              </div>
+            );
+          } else {
+            return (
+              <div key={index} onClick={() => handleCardClick(item)}>
+                <img
+                  className="lazy rounded-lg cursor-pointer"
+                  src={`https://cdn.midjourney.com/${item.jobid}/${item.index}.png`}
+                  alt={`${item.prompt}`}
+                  loading="lazy"
+                />
+              </div>
+            );
+          }
+        })}
+      </Masonry>
+      {selectedItem && (
+        <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
+          <div className="relative bg-stone-100 p-5 rounded-lg max-w-3xl w-full">
+            <button className="absolute top-5 right-5 text-stone-900/80 font-bold" onClick={handleClose}>Close</button>
+            <img width="50%"
+              src={`https://cdn.midjourney.com/${selectedItem.jobid}/${selectedItem.index}.png`}
+              alt={`${selectedItem.prompt}`}
+            />
+            <p className="mt-4 text-xs text-stone-900/80">{selectedItem.prompt}</p>
+          </div>
+        </div>
+      )}
+      <div className="my-10 p-5 bg-stone-900/10 rounded-lg text-right">
+        <div className="mt-20 text-2xl font-extrabold text-stone-900/80">intelli.<span className="text-stone-900/40">show</span></div>
+        <div className="mb-2 text-xs text-stone-900/40">&copy; 2024-2025, Kevin Gao @ Toronto</div>
+      </div>
+    </main>   
   );
 }
