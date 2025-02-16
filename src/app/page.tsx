64f5bbe23dd32c15c8f18a55ee0c3389b6a76ed6
Beijing Uncle <kevin.p.gao@gmail.com>
2025-02-15 19:16:34 -0500

    20250215-3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,23 @@ import Masonry from "react-masonry-css";
 
 const MAX_PAGES = 6; // 设置最大页面数常量
 
+interface Item {
+  jobid: string;
+  index: number;
+  prompt: string;
+}
+
 export default function Home() {
-  const [items, setItems] = useState<any[]>([]);
+  const [items, setItems] = useState<Item[]>([]);
   const [page, setPage] = useState(MAX_PAGES);
   const [hasMore, setHasMore] = useState(true);
-  const [selectedItem, setSelectedItem] = useState(null);
+  const [selectedItem, setSelectedItem] = useState<Item | null>(null);
   const observer = useRef<IntersectionObserver | null>(null);
 
   useEffect(() => {
     async function fetchData(page: number) {
       const response = await fetch(`/data/page_${page}.json`); // 替换为你的 API 端点
-      const data = await response.json();
+      const data: Item[] = await response.json();
       setItems(prevItems => [...prevItems, ...data]);
       if (page < 2) {
         setHasMore(false);
@@ -25,7 +31,7 @@ export default function Home() {
     fetchData(page);
   }, [page]);
 
-  const lastItemRef = useCallback(node => {
+  const lastItemRef = useCallback((node: HTMLDivElement) => {
     if (observer.current) observer.current.disconnect();
     observer.current = new IntersectionObserver(entries => {
       if (entries[0].isIntersecting && hasMore) {
@@ -42,7 +48,7 @@ export default function Home() {
     640: 2
   };
 
-  const handleCardClick = (item) => {
+  const handleCardClick = (item: Item) => {
     setSelectedItem(item);
   };
 
@@ -54,8 +60,7 @@ export default function Home() {
     <main className="py-5 px-5 sm:px-10 md:px-15 lg:px-20">
       <Masonry breakpointCols={breakpointColumnsObj} className="my-masonry-grid" columnClassName="my-masonry-grid_column">
         <div key="intelli.show" className='p-5 bg-stone-900/80 text-stone-100/80 rounded-lg'>
-          <div className='mt-2 text-xl font-bold'>imagined: <span className='text-stone-100/50'>reality</span></div>
-          <div className='mb-20 text-xs text-stone-100/30'>niji・journey</div>
+          <div className='mt-2 mb-20 text-xl font-bold'>imagined: <span className='text-stone-100/50'>reality</span></div>
           <div className='text-right text-xs text-stone-100/30'>kevin.p.gao@gmail.com</div>
         </div>
         {items.map((item, index) => {
@@ -88,7 +93,8 @@ export default function Home() {
         <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
           <div className="relative bg-stone-100 p-5 rounded-lg max-w-3xl w-full">
             <button className="absolute top-5 right-5 text-stone-900/80 font-bold" onClick={handleClose}>Close</button>
-            <img width="50%"
+            <img
+              width="50%"
               src={`https://cdn.midjourney.com/${selectedItem.jobid}/${selectedItem.index}.png`}
               alt={`${selectedItem.prompt}`}
             />
